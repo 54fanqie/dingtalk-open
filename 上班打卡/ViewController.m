104550fdd80408506e3320f8d9647c9f5b359fa3
番茄 <fanqie@fanqiedemacbook-pro.local>
2018-01-24 16:30:49 +0800

    Initial Commit

---
+++ 上班打卡/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  上班打卡
+//
+//  Created by 番茄 on 1/24/18.
+//  Copyright © 2018 番茄. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
